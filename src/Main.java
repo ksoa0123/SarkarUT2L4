7fe10cd1512f5adc7ac3925728df51d5c193b192
Koushik Sarkar <koushiks@nycstudents.net>
2022-10-28 11:53:13 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = myScanner.nextInt();
+
+        // write your code here to print the appropriate message(s)
+        // described below
+
+    }
+}
